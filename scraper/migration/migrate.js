af673da2c410a0665b7ed3f042ae8be70e15f63f
Vincent Will <vincent.will@onlogist.com>
2024-09-03 15:25:12 +0200

    update migrate

--- scraper/migration/migrate.js
+++ scraper/migration/migrate.js
@@ -4,13 +4,13 @@ import { parseFeatures } from '../src/utils/parseFeatures.js';
 // todo migration remove without searchUrl
 
 const run = async () => {
-  const client = new MongoClient('mongodb+srv://admin:lna5Cq5oRCspKze8@dev.7ndpuku.mongodb.net/?retryWrites=true&w=majority&appName=dev');
+  const client = new MongoClient('here');
   await client.connect();
   const db = client.db('prod');
   const collection = db.collection('estates');
 
   // delete all immoscout entries
-  await collection.deleteMany({ provider: 'immobilienscout24.de' });
+  // await collection.deleteMany({ provider: 'immobilienscout24.de' });
 
   client.close();
 }
