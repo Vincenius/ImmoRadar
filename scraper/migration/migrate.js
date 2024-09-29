51fd0af771542a86302162940fb2222674074dbe
Vincenius <vincent.will@vaitrade.de>
2024-09-29 08:55:02 +0200

    update immoscout urls

--- scraper/migration/migrate.js
+++ scraper/migration/migrate.js
@@ -4,13 +4,13 @@ import { parseFeatures } from '../src/utils/parseFeatures.js';
 // todo migration remove without searchUrl
 
 const run = async () => {
-  const client = new MongoClient('yoyo');
+  const client = new MongoClient('mongodb://vincenius:6fc07b5d3d824f4d9f22284f9b959320@217.72.202.55:27017/?authSource=admin');
   await client.connect();
   const db = client.db('prod');
   const collection = db.collection('estates');
 
   // delete all immoscout entries
-  const result = await collection.deleteMany({ provider: 'immowelt.de' });
+  const result = await collection.deleteMany({ provider: 'immobilienscout24.de' });
   console.log(result)
 
   client.close();
