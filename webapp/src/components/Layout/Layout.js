d5dd071945d2ba64677cc75bec6c2b3cac8747b1
Vincent Will <vincent.will@onlogist.com>
2025-01-14 15:43:19 +0100

    update links

--- webapp/src/components/Layout/Layout.js
+++ webapp/src/components/Layout/Layout.js
@@ -12,7 +12,7 @@ import styles from './Layout.module.css';
 const menu = [
   {
     label: 'Grundstückbörse',
-    url: '/grundstueckboerse'
+    url: '/grundstueckboerse/suchen'
   },
   // {
   //   label: 'Förderungscheck',
