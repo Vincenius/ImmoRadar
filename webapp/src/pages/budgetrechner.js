f4963e5a6a7e483f8e329d549a8c3ccdcc7310f2
Vincent Will <vincent.will@onlogist.com>
2025-01-14 13:09:03 +0100

    updates on budget calculator

--- webapp/src/pages/budgetrechner.js
+++ webapp/src/pages/budgetrechner.js
@@ -1,19 +1,34 @@
 import React, { useState, useRef } from 'react';
+import { useDisclosure } from '@mantine/hooks';
 import Link from 'next/link';
 import NextImage from 'next/image';
 import Layout from '@/components/Layout/Layout';
-import { Title, Text, Card, NumberInput, NumberFormatter, Box, Flex, Image, Button, ActionIcon, Tooltip } from '@mantine/core';
+import { Title, Text, Card, NumberInput, NumberFormatter, Box, Flex, Image, Button, ActionIcon, Popover, Slider } from '@mantine/core';
 import { IconShare3, IconQuestionMark } from '@tabler/icons-react';
 import styles from '@/styles/Home.module.css'
 
 const CustomLabel = ({ label, desciption }) => {
+  const [opened, { close, open }] = useDisclosure(false);
+
   return <Flex gap="xs" align="center">
     <Text>{label}</Text>
-    <Tooltip label={desciption}>
-      <ActionIcon variant="outline" radius="xl" aria-label="Erklärung" size="xs">
-        <IconQuestionMark style={{ width: '70%', height: '70%' }} stroke={1.5} />
-      </ActionIcon>
-    </Tooltip>
+    <Popover width={300} position="bottom" withArrow shadow="md" opened={opened}>
+      <Popover.Target>
+        <ActionIcon
+          variant="outline" radius="xl" aria-label="Erklärung" size="xs"
+          onMouseEnter={open} onMouseLeave={close}
+          onClick={() => {
+            if (opened) { close() }
+            else { open() }
+          }}
+        >
+          <IconQuestionMark style={{ width: '70%', height: '70%' }} stroke={1.5} />
+        </ActionIcon>
+      </Popover.Target>
+      <Popover.Dropdown style={{ pointerEvents: 'none' }}>
+        <Text size="sm">{desciption}</Text>
+      </Popover.Dropdown>
+    </Popover>
   </Flex>
 }
 
@@ -27,12 +42,12 @@ const BudgetCalculator = () => {
   const calculateAnnuity = () => {
     const monthlyInterest = ((interest || 0) / 100) / 12;
     const months = (years || 1) * 12;
-    
+
     const loanAmount = (installment * (1 - Math.pow(1 + monthlyInterest, -months)) / monthlyInterest) + (equity || 0)
-    
+
     return loanAmount.toFixed(2);
   }
-  
+
 
   return (
     <Layout title="Budgetrechner | ImmoRadar" description="Berechnen Sie schnell und einfach, wie viel Ihr Haus kosten darf. Unser Budgetrechner kalkuliert anhand von Eigenkapital, Zinssatz und monatlicher Belastung den maximalen Kaufpreis, den Sie sich leisten können.">
@@ -42,8 +57,8 @@ const BudgetCalculator = () => {
             <span className={styles.gradientText}>Budgetrechner</span>
           </Title>
           <Text size="lg" mb="xl" ta={{ base: 'center', sm: 'left' }}>
-            Berechne schnell und einfach, wie viel Dein Haus kosten darf. Unser Budgetrechner kalkuliert anhand von Eigenkapital,
-            Finanzierungszins und der Wunschrate den maximalen Kaufpreis, den Du dir leisten kannst.
+            Berechne schnell und einfach, wie viel Dich Dein Traumhaus kosten darf.
+            Unser Budgetrechner kalkuliert anhand von Eigenkapital, Finanzierungszins und deiner persönlichen Wunschrate.
           </Text>
           <Button
             variant="outline"
@@ -66,66 +81,109 @@ const BudgetCalculator = () => {
       </Flex>
 
       <Card mb="xl" ref={scollToRef}>
-        <NumberInput
-          label={<CustomLabel label="Dein Eigenkapital" desciption="Welche Summe an Kapital kannst Du in Dein Projekt Traumimmobilie mit einbringen" />}
-          placeholder="50 000€"
-          decimalScale={0}
-          min={0}
-          thousandSeparator=" "
-          suffix="€"
-          mb="md"
-          value={equity}
-          onChange={(value) => setEquity(value)}
-        />
+        <Flex justify="space-between" gap="md" direction={{ base: "column", xs: "row" }}>
+          <Box w={{ base: "100%", xs: "67%" }}>
+            <NumberInput
+              label={<CustomLabel label="Dein Eigenkapital" desciption="Welche Summe an Kapital kannst Du in Dein Projekt Traumimmobilie mit einbringen" />}
+              placeholder="50 000€"
+              decimalScale={0}
+              min={0}
+              thousandSeparator=" "
+              suffix="€"
+              mb="md"
+              value={equity}
+              onChange={(value) => setEquity(parseInt(value))}
+            />
 
-        <NumberInput
-          label={<CustomLabel label="Deine Wunschrate" desciption="Welche monatlichen „Ab- Sparrate“ für die Finanzierung ist für Dich ideal?" />}
-          placeholder="1 200€"
-          decimalScale={0}
-          min={100}
-          thousandSeparator=" "
-          suffix="€"
-          mb="md"
-          value={installment}
-          onChange={(value) => setInstallment(value)}
-        />
+            <CustomLabel label="Deine Wunschrate" desciption="Welche monatlichen „Ab- Sparrate“ für die Finanzierung ist für Dich ideal?" />
+            <Slider
+              my="xl"
+              labelAlwaysOn
+              size="xl"
+              decimalScale={0}
+              min={100}
+              max={5000}
+              step={100}
+              thousandSeparator=" "
+              suffix="€"
+              label={(value) => <NumberFormatter
+                suffix='€'
+                value={value}
+                thousandSeparator=" "
+                decimalScale={0}
+              />}
+              mb="md"
+              value={installment}
+              onChange={(value) => setInstallment(value)}
+            />
 
-        <NumberInput
-          label={<CustomLabel label="Finanzierungszins" desciption="Hier stellst Du den Zins für die Finanzierung ein." />}
-          placeholder="3%"
-          decimalSeparator=","
-          decimalScale={1}
-          min={1}
-          max={4}
-          thousandSeparator=" "
-          suffix="%"
-          mb="md"
-          value={interest}
-          onChange={(value) => setInterest(value)}
-        />
+            <CustomLabel label="Finanzierungszins" desciption="Hier stellst Du den Zins für die Finanzierung ein." />
+            <Slider
+              my="xl"
+              labelAlwaysOn
+              size="xl"
+              decimalScale={1}
+              min={1}
+              max={5}
+              step={0.1}
+              thousandSeparator=" "
+              suffix="€"
+              label={(value) => <NumberFormatter
+                suffix='%'
+                value={value}
+                decimalScale={1}
+              />}
+              mb="md"
+              value={interest}
+              onChange={(value) => setInterest(value)}
+            />
 
-        <NumberInput
-          label={<CustomLabel label="Schuldenfrei in" desciption="Wann soll Deine Traumimmobilie komplett Dir gehören?" />}
-          placeholder="30 Jahren"
-          decimalScale={0}
-          thousandSeparator=" "
-          min={1}
-          max={35}
-          suffix=" Jahren"
-          mb="md"
-          value={years}
-          onChange={(value) => setYears(value)}
-        />
+            <CustomLabel label="Schuldenfrei in" desciption="Wann soll Deine Traumimmobilie komplett Dir gehören?" />
+            <Slider
+              my="xl"
+              labelAlwaysOn
+              size="xl"
+              decimalScale={0}
+              min={5}
+              max={40}
+              step={1}
+              thousandSeparator=" "
+              suffix=" Jahren"
+              label={(value) => <NumberFormatter
+                suffix=' Jahren'
+                value={value}
+                decimalScale={0}
+              />}
+              mb="md"
+              value={years}
+              onChange={(value) => setYears(value)}
+            />
+          </Box>
 
-        <Text fw={500} mt="md" mb="sm" size="lg">Gesamtbudget für Dein Projekt „Traumhaus“:</Text>
-        <Text c="green.9" fw={600} size="2.4em" lh="1em" mb="xl" td="underline">
-          <NumberFormatter
-            suffix='€'
-            value={calculateAnnuity()}
-            thousandSeparator=" "
-            decimalScale={0}
-          />
-        </Text>
+          <Box w={{ base: "100%", xs: "33%"}} ta={{ base: 'left', xs: 'center' }}>
+            <Image
+              component={NextImage}
+              src="/imgs/house-logo.png"
+              alt="Minimalistisches Haus"
+              height={200}
+              width={200}
+              w={200}
+              mb="md"
+              mx="auto"
+              display={{ base: "none", xs: "block" }}
+            />
+            <Text fw={500} mt="md" mb="sm" size="lg">Gesamtbudget für Dein Projekt „Traumhaus“:</Text>
+            <Text c="green.9" fw={600} size="2.4em" lh="1em" mb="xl" td="underline">
+              <NumberFormatter
+                suffix='€'
+                value={calculateAnnuity()}
+                thousandSeparator=" "
+                decimalScale={0}
+              />
+            </Text>
+          </Box>
+
+        </Flex>
 
         <Link href="/suche">
           <Card radius="md" p="md" bg="cyan.0">
@@ -134,7 +192,7 @@ const BudgetCalculator = () => {
                 <Text fw={500} c="black">ImmoRadar Grundstückbörse</Text>
                 <Text>Entdecke exklusive Grundstücke, die du sonst nirgendwo findest.</Text>
               </Box>
-              <IconShare3 c="gray.7"/>
+              <IconShare3 c="gray.7" />
             </Flex>
           </Card>
         </Link>
