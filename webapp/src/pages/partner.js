ae2d42da7b18c07da23e20017759e8f735a9ed30
Vincent Will <vincent.will@onlogist.com>
2025-01-30 08:19:57 +0100

    add partner page

---
+++ webapp/src/pages/partner.js
@@ -0,0 +1,82 @@
+import React from 'react';
+import { Container, Title, Text, Grid, Image, Card, Flex, Box } from '@mantine/core';
+import NextImage from 'next/image';
+import Layout from '@/components/Layout/Layout';
+
+const Partner = ({ image, name, description, link, linkText }) => {
+  return (<Grid.Col span={12}>
+    <Card shadow="sm" withBorder>
+      <Flex gap="lg" direction={{ base: 'column', xs: 'row' }}>
+        <Image
+          component={NextImage}
+          src={`/imgs/partner/${image}`}
+          height={300}
+          width={300}
+          w={{ base: '100%', xs: 150 }}
+          h={{ base: 'auto' }}
+          alt={name}
+        />
+
+        <Flex direction="column" justify="space-between">
+          <Box>
+            <Title order={3} size="h4" mb="sm">{name}</Title>
+            <Text fs="italic" mb="xs">{description}</Text>
+          </Box>
+          <Text><a href={link} target='_blank' rel="noopener noreferrer">{linkText}</a></Text>
+        </Flex>
+      </Flex>
+    </Card>
+  </Grid.Col>)
+}
+
+const PartnerPage = () => {
+  return (
+    <Layout
+      title="Kooperationspartner | ImmoRadar"
+    // TODO description="Willkommen bei ImmoRadar, Ihrer Webseite für die Wohnungssuche in Deutschland. Diese Webseite wurde von mir, Vincent, ins Leben gerufen. Mein Ziel ist es, die Wohnungssuche so einfach und stressfrei wie möglich zu gestalten..."
+    >
+      <Container py="xl" size="sm">
+        <Title order={1} mb="lg">Kooperations<wbr/>partner</Title>
+        <Text mb="xl">
+          <b>Einfach Finden.</b> Unsere starken Partner an unserer Seite - für eine erfolgreiche und innovative Immobiliensuche.
+        </Text>
+
+        <Grid mb="3rem">
+          <Partner
+            name="tinyways"
+            image="tinyways.png"
+            link="https://www.tinyways.de/"
+            linkText="tinyways.de"
+            description="Eine Handvoll Expert:innen von ganz unterschiedlichen Disziplinen, welche seit Anfang 2020 die Idee von einer ökologischen tiny house-Gemeinschaft haben."
+          />
+
+          <Partner
+            name="Andre Lamberti"
+            image="swisslife-andre.jpg"
+            link="https://swisslife-select.finlink.de/andre-lamberti/start/finance_type?partner=3c37fba5-673b-4fef-bfd9-95c11ae8aa68&partnerCompany=Oliver%20Rausch"
+            linkText="swisslife-select.finlink.de"
+            description="Andre Lamberti ist Berater bei SwissLife Select. Er unterstützt Kunden bei Immobilienfinanzierung und Vermögensplanung, um langfristige finanzielle Sicherheit zu gewährleisten."
+          />
+
+          <Partner
+            name="Baufinanzierung Horn & Rozanski"
+            image="hr-baufinanzierung.png"
+            link="https://www.hr-baufinanzierung.de/"
+            linkText="hr-baufinanzierung.de"
+            description="Baufinanzierung Horn & Rozanski bietet transparente, maßgeschneiderte Baufinanzierungen und arbeitet mit über 600 Banken zusammen, um Kunden erfolgreich zur Traumimmobilie zu begleiten."
+          />
+        </Grid>
+
+        <Title order={2} mb="sm">Werde Partner!</Title>
+        <Text mb="sm">
+          Hast du Interesse an einer Zusammenarbeit? Kontaktiere uns und lass uns gemeinsam neue Wege in die Zukunft gehen.
+        </Text>
+        <Text mb="xl">
+          <a href="mailto:oliver.rausch@immoradar.xyz">oliver.rausch@immoradar.xyz</a>
+        </Text>
+      </Container>
+    </Layout>
+  );
+};
+
+export default PartnerPage;
