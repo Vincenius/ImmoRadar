66c729fc9382f0d55e46ded3e8d4a3558cfc639c
Vincent Will <vincent.will@onlogist.com>
2025-05-25 16:09:59 +0200

    improve quote slider

--- foerderung/src/components/QuoteSlider/QuoteSlider.js
+++ foerderung/src/components/QuoteSlider/QuoteSlider.js
@@ -1,33 +1,33 @@
 import { useRef } from 'react';
 import { Carousel } from '@mantine/carousel';
-import { Blockquote, Text } from '@mantine/core';
-import { IconQuote } from '@tabler/icons-react';
+import { Blockquote, Box } from '@mantine/core';
 import Autoplay from 'embla-carousel-autoplay';
 import classes from './QuoteSlider.module.css'
 
 function QuoteSlider({ quotes }) {
   const autoplay = useRef(Autoplay({ delay: 2000 }));
   return (
-    <Carousel
-      height={250}
-      controlSize={20}
-      slideSize={{ base: '90%', sm: '70%', md: '33.333333%' }}
-      slideGap={{ base: 'md', sm: 'xl' }}
-      style={{ overflow: 'visible' }}
-      align="center"
-      plugins={[autoplay.current]}
-      onMouseEnter={autoplay.current.stop}
-      onMouseLeave={autoplay.current.reset}
-      classNames={classes}
-    >
-      {quotes.map((quote, index) => (
-        <Carousel.Slide key={`quote-${index}`}>
-          <Blockquote h="100%" icon={<IconQuote />} radius="sm" color="cyan" cite={quote.author} w="100%" styles={{ root: { backgroundColor: 'var(--mantine-primary-color-0)' } }}>
-            <Text align="left">{quote.text}</Text>
-          </Blockquote>
-        </Carousel.Slide>
-      ))}
-    </Carousel>
+    <Box>
+      <Carousel
+        height={250}
+        controlSize={20}
+        slideSize={{ base: '90%', sm: '70%', md: '33.333333%' }}
+        slideGap={{ base: 'md', sm: 'xl' }}
+        align="center"
+        plugins={[autoplay.current]}
+        onMouseEnter={autoplay.current.stop}
+        onMouseLeave={autoplay.current.reset}
+        classNames={classes}
+      >
+        {quotes.map((quote, index) => (
+          <Carousel.Slide key={`quote-${index}`}>
+            <Blockquote h="100%" radius="sm" color="cyan" cite={quote.author} w="100%" styles={{ root: { backgroundColor: 'var(--mantine-primary-color-0)' } }}>
+              {quote.text}
+            </Blockquote>
+          </Carousel.Slide>
+        ))}
+      </Carousel>
+    </Box>
   )
 }
 
